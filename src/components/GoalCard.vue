1fd667fda0e0836e61f6e2fb488c2905e84c409e
Chase Williams <chase.williams@tangocard.com>
2022-10-01 09:27:29 -0600

    fixed edit and added remove button

--- src/components/GoalCard.vue
+++ src/components/GoalCard.vue
@@ -1,21 +1,43 @@
 <template>
-  <v-card class="mt-2 mb-2">
+  <v-card class="mt-4 mb-4">
     <div v-if="!editing">
-      <v-card-title>
-        {{ goalTitle }}
-      </v-card-title>
-      <v-card-subtitle>
-        {{ goalDescription }}
-      </v-card-subtitle>
-      <v-btn
-          @click="editGoal"
-          elevation="0"
-          class="transparent"
-      >
-        <v-icon>
-          mdi-pencil
-        </v-icon>
-      </v-btn>
+      <v-row class="justify-space-between">
+        <v-col
+            cols="8"
+        >
+          <v-card-title>
+            {{ goalTitle }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ goalDescription }}
+          </v-card-subtitle>
+        </v-col>
+        <v-col
+            class="text-right"
+            cols="4"
+        >
+          <v-btn
+              @click="editGoal"
+              elevation="0"
+              class="transparent pa-1"
+          >
+            <v-icon>
+              mdi-pencil
+            </v-icon>
+          </v-btn>
+          <v-btn
+              @click="editGoal"
+              elevation="0"
+              class="transparent pa-1 mr-2"
+          >
+            <v-icon>
+              mdi-close
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+
     </div>
 
     <div v-else class="pa-4">
@@ -88,6 +110,9 @@ export default {
 <style lang="scss" scoped>
 .v-icon {
   border: none
+}
+.v-btn {
+  min-width: 30px !important;
 }
 </style>
 
